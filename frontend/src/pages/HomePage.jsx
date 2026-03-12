dc7de9418fb825a426f33dc976a3737e80807dde
Abhijit Nath <abhijitnath1a1@gmail.com>
2026-03-12 18:56:08 +0530

    rate limit ui created

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
 import Navbar from '../components/Navbar';
+import RateLimitUI from '../components/RateLimitUI';
 
 export default function HomePage() {
+  const [isRateLimited, setIsRateLimited] = useState(true);
+
   return (
     <div className='min-h-screen'>
       <Navbar />
+
+      {isRateLimited && <RateLimitUI />}
     </div>
   );
 }
