f8e738a0c6a6828d4cb07cb3ad14b5e0b3bea4f8
Abhijit Nath <abhijitnath1a1@gmail.com>
2026-03-12 17:52:02 +0530

    middleware for ratelimit

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,26 @@ import dotenv from 'dotenv';
 import express from 'express';
 import noteRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
 
-connectDB();
+// trust proxy setting
+app.set('trust proxy', 1);
+
+const PORT = process.env.PORT || 5001;
 
 // parsing middleware
 app.use(express.json());
+// custom middleware
+app.use(rateLimiter);
 
 app.use('/api/notes', noteRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server started on PORT:${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT:${PORT}`);
+  });
 });
