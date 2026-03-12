28e0150eebfec4de554526b5a0861f41539c38e3
Abhijit Nath <abhijitnath1a1@gmail.com>
2026-03-12 18:27:55 +0530

    setup react project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import NoteDetailPage from './pages/NoteDetailPage';
+
+export default function App() {
+  return (
+    <div data-theme='valentine'>
+      <button className='btn btn-outline'>click Me</button>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+        <Route path='/note/:id' element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+}
