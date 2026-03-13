ff3536466a46e65c7931818df5ed7f43d0cf4c13
Abhijit Nath <abhijitnath1a1@gmail.com>
2026-03-13 10:55:26 +0530

    no note found UI

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitUI from '../components/RateLimitUI';
 import toast from 'react-hot-toast';
 import NoteCard from '../components/NoteCard';
 import api from '../lib/axios';
+import NotesNotFound from '../components/NotesNotFound';
 
 export default function HomePage() {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -38,9 +39,15 @@ export default function HomePage() {
       {isRateLimited && <RateLimitUI />}
 
       <div className='max-w-7xl mx-auto p-4 mt-6'>
+        {/* Initial loading before UI render */}
         {loading && (
           <div className='text-center text-primary py-10'>Loading...</div>
         )}
+
+        {/* If no notes created yet */}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
+        {/* Render all notes */}
         {notes.length > 0 && !isRateLimited && (
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {notes.map((note) => (
